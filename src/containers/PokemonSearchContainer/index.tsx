4f824ed77e849c861c9ac041b552b62e11cb8bd1
isnendyankp <isnendyankp@gmail.com>
2024-01-09 15:43:35 +0700

    add style card for pokemon after success search

--- src/containers/PokemonSearchContainer/index.tsx
+++ src/containers/PokemonSearchContainer/index.tsx
@@ -38,18 +38,21 @@ const PokemonSearchContainer: React.FC = () => {
     <div>
       <input
         type="text"
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
+        onChange={(e) => setSearch(e.target.value)}
         value={search}
         placeholder="Search Pokemon"
+        className="p-2 rounded border border-gray-300 shadow-inner mb-4"
       />
       {pokemon && (
-        <div>
-          <h1>{pokemon.name}</h1>
-          {pokemon.sprites && (
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-          )}
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            className="w-full"
+          />
+          <div className="px-6 py-4">
+            <h1 className="font-bold text-xl mb-2">{pokemon.name}</h1>
+          </div>
         </div>
       )}
       <button
