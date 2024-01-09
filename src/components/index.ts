7155238f21f5c4e391bec77623826a77713d2abd
isnendyankp <isnendyankp@gmail.com>
2024-01-09 14:25:57 +0700

    Create file distributor

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+//  Create file distributor
+
+export { default as Input } from './Input';
+export { default as Text } from './Text';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Navbar } from './Navbar';
+
