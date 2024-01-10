48db64bf175f62976ed015e0f6621e7743daa589
isnendyankp <isnendyankp@gmail.com>
2024-01-10 14:23:25 +0700

    Fix pokemon search,

--- src/containers/PokemonSearchContainer/index.tsx
+++ src/containers/PokemonSearchContainer/index.tsx
@@ -19,9 +19,19 @@ const PokemonSearchContainer: React.FC = () => {
   const searchPokemon = async () => {
     try {
       const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`
+        `https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000`
       );
-      setPokemon(response.data);
+      const foundPokemon = response.data.results.find(
+        (poke: { name: string }) => poke.name.includes(search.toLowerCase())
+      );
+      if (foundPokemon) {
+        const pokemonResponse = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${foundPokemon.name}`
+        );
+        setPokemon(pokemonResponse.data);
+      } else {
+        setPokemon(null);
+      }
     } catch (error) {
       console.error('Error: Pokemon not found');
     }
@@ -37,14 +47,14 @@ const PokemonSearchContainer: React.FC = () => {
 
   return (
     <div>
-      <input
+      <Input
         type="text"
         onChange={(e) => setSearch(e.target.value)}
         value={search}
         placeholder="Search Pokemon"
         className="p-2 rounded border border-gray-300 shadow-inner mb-4"
       />
-      {pokemon && (
+      {pokemon ? (
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
           <img
             src={pokemon.sprites.front_default}
@@ -55,6 +65,8 @@ const PokemonSearchContainer: React.FC = () => {
             <h1 className="font-bold text-xl mb-2">{pokemon.name}</h1>
           </div>
         </div>
+      ) : (
+        <p>No Pokemon found.</p>
       )}
       <button
         onClick={() => navigate('/pokemonList')}
