3fead4d0bf6f71f3106ddbddcbdd627b146ddbe9
isnendyankp <isnendyankp@gmail.com>
2024-01-12 18:21:53 +0700

    remove  regis login crud junk container

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,2 @@
-export { default as HomeContainer } from './HomeContainer';
-export { default as LoginContainer } from './LoginContainer';
-export { default as ListContainer } from './ListContainer';
-export { default as CreateCategoryContainer } from './CreateCategoryContainer';
 export { default as PokemonListContainer } from './PokemonListContainer';
 export { default as PokemonSearchContainer } from './PokemonSearchContainer';
