7a3614231b132a135abffb5aade4dbdf57e0c919
isnendyankp <isnendyankp@gmail.com>
2024-01-15 13:45:01 +0700

    add file distributor for LoginContainer

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeContainer } from './HomeContainer';
+export { default as LoginContainer } from './LoginContainer';
 export { default as PokemonListContainer } from './PokemonListContainer';
 export { default as PokemonSearchContainer } from './PokemonSearchContainer';
